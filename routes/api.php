4df54967f7f2e5a99a6075ca90c49f728d2e1789
Andrew <feshykandrew.work@gmail.com>
2022-05-08 15:57:33 +0300

    Auth init

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\StoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
+    Route::post('/', [StoreController::class, '__invoke']);
+});
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', '\App\Http\Controllers\AuthController@login');
+    Route::post('logout', '\App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', '\App\Http\Controllers\AuthController@refresh');
+    Route::post('me', '\App\Http\Controllers\AuthController@me');
+
+    Route::group(['middleware' => 'jwt.auth' ], function () {
+        Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function () {
+//            Route::get('/', [\App\Http\Controllers\Fruit\IndexController::class, '__invoke']);
+        });
+    });
 });
